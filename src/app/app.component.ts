07092bf2a204b59d3230ae064fa2775f6e5b6d94
Kati0405 <katirin90@gmail.com>
2022-02-03 16:46:40 +0200

    Lesson 2 ht

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
 
+enum Race {elf="elf", human="human", dwarf="dwarf", gnome="gnome", troll="troll"}
+enum Sex {male="M", female="F"}
+
+interface ICharacter {
+    name: string;
+    race: Race;
+    sex: Sex;
+    }
+
+enum Profession {archer="archer", warrior="warrior", priest="priest", mage="mage", druid="druid"}
+
+class Character implements ICharacter {
+    name: string;
+    race: Race;
+    sex: Sex;
+    profession: Profession;
+    level: number;
+
+    constructor(name: string, race: Race, sex: Sex, profession: Profession, level: number){
+        this.name = name,
+        this.race=race,
+        this.sex = sex,
+        this.profession = profession,
+        this.level = level
+    }
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
+  
   title = 'lesson2-ht';
+  name = "Your name";
+  age = "Your age"
+
+  listOfChars = [
+
+  new Character("Qiqi", Race.human, Sex.female, Profession.priest, 1),
+  new Character("Amber", Race.elf, Sex.female, Profession.archer, 10),
+  new Character("Razor", Race.troll, Sex.male, Profession.warrior, 5)
+  ]
+
+  activeChar: Character = this.listOfChars[0]
+
+  
+  color: string = 'red';
+
+  listOfColors = ['red', 'blue', 'green', 'black']
+
+  
+
+  text = false;
+  handleClick(){
+    this.text = !this.text;
+    console.log(this.activeChar)
+  }
+
+  pickRandomPhrase(): void {
+    let phrases = ['Yes', 'No', 'Maybe', 'Not likely', 'Absolutely no', 'Great idea'];
+    let phrase = phrases[Math.floor(Math.random()*phrases.length-1)];
+    
+    alert(phrase);
+
+   
+
+  }
 }
